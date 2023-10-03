7c583f9bcc65763178488a27fce6f63fceffecad
BrunoSouza <desouza88@hotmail.com>
2023-10-02 22:18:23 -0300

    feat: Day 24 profit or loss  challenge done

---
+++ Coding Exercises/Java/Day24/Exercise.java
@@ -0,0 +1,7 @@
+public class Exercise {
+  public static double calculateProfitOrLoss(double costPrice, double sellingPrice) {
+      double result = sellingPrice - costPrice;
+      
+      return result;
+  }
+}
---
+++ Coding Exercises/Python/Day24/calculate_profit_loss.py
@@ -0,0 +1,3 @@
+def calculate_profit_or_loss(cost_price, selling_price):
+    result = selling_price - cost_price
+    return result
